c469d7c1f33d757f41fdedeb31356ca7f8096f3d
Philipp Tkachev <zoonman@gmail.com>
2014-12-14 15:26:35 -0500

    initial commit

---
+++ examples/index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * pixabay-php-api
+ * index.php
+ *
+ * PHP Version 5
+ *
+ * @category Production
+ * @package  Default
+ * @author   Philipp Tkachev <philipp@marketmesuite.com>
+ * @date     12/14/14 9:58 AM
+ * @license  http://marketmesuite.com/license.txt MMS License
+ * @version  GIT: 1.0
+ * @link     http://marketmesuite.com/
+ */
+ 
---
+++ src/Zoonman/Pixabay/PixabayClient.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * pixabay-php-api
+ * PixabayClient API
+ *
+ * PHP Version 5
+ *
+ * @category Production
+ * @package  Default
+ * @author   Philipp Tkachev <zoonman@gmail.com>
+ * @date     12/14/14 9:18 AM
+ * @license  https://www.zoonman.com/projects/pixabay/license.txt MIT
+ * @version  GIT: 1.0
+ * @link     https://www.zoonman.com/projects/pixabay/
+ */
+ 
+
+namespace Zoonman\Pixabay;
+
+use GuzzleHttp\Client;
+
+/**
+ * Class PixabayClient
+ * @package Zoonman\PixabayClient
+ */
+class PixabayClient {
+    /**
+     * @var array
+     */
+    private $options =[];
+    /**
+     * @var array
+     */
+    private $optionsList = [
+        'username',
+        'key',
+        'response_group',
+        'id',
+        'q',
+        'lang',
+        'image_type',
+        'orientation',
+        'min_width',
+        'min_height',
+        'editors_choice',
+        'safesearch',
+        'order'
+    ];
+    /**
+     * @var Client
+     */
+    private $client;
+    /**
+     *
+     */
+    const API_ROOT = 'http://pixabay.com/api/';
+
+    /**
+     * @param array $options
+     * @throws \Exception
+     */
+    public function __construct(array $options)
+    {
+        $this->client = new Client(['base_url' => self::API_ROOT]);
+        if (empty($options['username'])) {
+            throw new \Exception('You must specify "username" parameter in constructor options');
+        }
+        if (empty($options['key'])) {
+            throw new \Exception('You must specify "key" parameter in constructor options');
+        }
+        $this->parseOptions($options);
+    }
+
+    /**
+     * @param array $options
+     * @param bool $resetOptions
+     */
+    private function parseOptions(array $options, $resetOptions = false) {
+        foreach ($this->optionsList as $option) {
+            if (isset($options[$option])) {
+                $this->options[$option] = $options[$option];
+            } elseif ($resetOptions && isset($this->options[$option])) {
+                unset($this->options[$option]);
+            }
+        }
+    }
+
+    /**
+     * @param array $options
+     * @param bool $returnObject
+     * @param bool $resetOptions
+     * @return mixed
+     */
+    public function get(array $options = [], $returnObject = false, $resetOptions = false) {
+
+        $this->parseOptions($options);
+        $response = $this->client->get(null, ['query' => $this->options]);
+
+        return $response->json(['object' => $returnObject]);
+    }
+}
